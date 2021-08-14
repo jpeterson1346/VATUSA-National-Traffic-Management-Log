5199f88b77dea46d5c2c8d88318969c4f0836eb9
Jeremy Peterson <jeremy.jeremy6294@gmail.com>
2021-08-14 16:42:05 -0400

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,8 +1,14 @@
 import discord
 
+intents = discord.Intents.default()
+
 channel_12345678 = 661596005191647233
+
+Client = discord.Client(intents = intents)
 
 token = open("token.txt", "r").read()
 client = discord.Client()
 
-GET /channels/channel_12345678/messages
+@client.event
+async def on_ready():
+print(‘Logged in as {0.user}’.format(client))
